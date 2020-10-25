42e2b4db830dc298512c58a47103020d666511a8
konglingwen94 <794430774@qq.com>
2020-10-25 19:58:12 +0800

    优化侧边栏布局

--- src/components/admin-menubar.vue
+++ src/components/admin-menubar.vue
@@ -12,18 +12,14 @@
           <i class="el-icon-location"></i>
           <span slot="title">导航一</span>
         </template>
-        <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
+        <el-menu-item index="1-1">
+          <i class="el-icon-setting"></i>
+          选项1
+        </el-menu-item>
+        <el-menu-item index="1-2">
+          <i class="el-icon-setting"></i>
+          选项2
+        </el-menu-item>
       </el-submenu>
       <el-menu-item index="2">
         <i class="el-icon-menu"></i>
@@ -46,7 +42,7 @@ export default {
   name: "admin-menubar",
   data() {
     return {
-      isCollapse: true
+      isCollapse: false
     };
   },
   created() {
@@ -68,6 +64,6 @@ export default {
 <style>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
-  min-height: 400px;
+  min-height: 800px;
 }
 </style>
