a19fd5e3852f130e76d2107859859c6446fce400
konglingwen94 <794430774@qq.com>
2020-10-25 19:12:22 +0800

    搭建页面容器布局

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <el-button>button</el-button>
-    <!-- <router-view/> -->
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -18,16 +19,21 @@
   color: #2c3e50;
 }
 
-// #nav {
-//   padding: 30px;
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+}
 
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+}
 
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 import "element-ui/lib/theme-chalk/index.css";
-import {Button} from 'element-ui'
+import { Header, Button, Container, Aside, Main } from "element-ui";
 
-Vue.use(Button)
-Vue.config.productionTip = false
+Vue.use(Button);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
