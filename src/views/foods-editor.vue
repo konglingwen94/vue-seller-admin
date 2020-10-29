7309159b3bfb283c3b7f5f3cfd4c7d8ffc42d650
konglingwen94 <794430774@qq.com>
2020-10-29 18:21:09 +0800

    添加商品分类选择框

--- src/views/foods-editor.vue
+++ src/views/foods-editor.vue
@@ -8,6 +8,16 @@
       <el-form-item label="名称">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
+      <el-form-item label="分类">
+        <el-select placeholder="请选择分类" v-model="form.menuID">
+          <el-option
+            v-for="item in categoryOpts"
+            :key="item.value"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="信息">
         <el-input v-model="form.info"></el-input>
       </el-form-item>
@@ -40,7 +50,6 @@
               $message.error(err.message);
             }
           "
-          :on-change="onUploadChange"
           :on-remove="
             () => {
               fileList = [];
@@ -54,17 +63,19 @@
         </el-upload>
       </el-form-item>
       <el-form-item>
-        <el-button @click="submit" type="primary">提交</el-button>
+        <el-button :loading="loading" @click="submit" type="primary">提交</el-button>
       </el-form-item>
     </el-form>
   </page-layout>
 </template>
 <script>
-import { fetchOneFoods, updateOneFoods } from "@/helper/request";
+import { fetchFoodsCategoryList, fetchOneFoods, updateOneFoods } from "@/helper/request";
 import { pick } from "@/helper/utils";
+import { log } from 'util';
 export default {
   name: "foods-editor",
   data: () => ({
+    categoryOpts: [],
     form: {
       name: "",
       oldPrice: 0,
@@ -72,8 +83,10 @@ export default {
       image: "",
       info: "",
       description: "",
+      menuID: "",
     },
     fileList: [],
+    loading: false,
   }),
   computed: {
     pageTitle() {
@@ -81,12 +94,21 @@ export default {
     },
   },
   created() {
+    fetchFoodsCategoryList()
+      .then((res) => {
+        this.categoryOpts = res;
+        // this.loading = false;
+      })
+      .catch((err) => {
+        this.$message.error(err.message);
+        // this.loading = false;
+      });
+
     if (this.pageTitle === "更新商品") {
       const id = this.$route.params.id;
       fetchOneFoods(id)
         .then((res) => {
           Object.assign(this.form, pick(res, Object.keys(this.form)));
-
           this.fileList = [
             {
               name: res.name,
@@ -101,7 +123,7 @@ export default {
   },
   methods: {
     submit() {
-      const { name, image, info, description, oldPrice, price } = this.form;
+      const { name, image, info, description, oldPrice,menuID, price } = this.form;
 
       if (!name) {
         this.$message.error("请输入名称");
@@ -115,12 +137,16 @@ export default {
       //     this.$message.error("请输入商品描述");
       //     return;
       //   }
+      if (!menuID) {
+        this.$message.error("请选择商品分类");
+        return;
+      }
       if (!image) {
         this.$message.error("请上传商品缩略图");
         return;
       }
       const id = this.$route.params.id;
-      const payload = { name, info, description, image, oldPrice, price };
+      const payload = {menuID, name, info, description, image, oldPrice, price };
       this.loading = true;
       updateOneFoods(id, payload)
         .then((res) => {
@@ -133,11 +159,6 @@ export default {
           this.loading = false;
         });
     },
-    onUploadSuccess() {},
-    onUploadChange(fileList, file, res) {},
-    // onUploadRemove(fileList, file, res) {
-    //     this.fileList=
-    // },
   },
 };
 </script>
