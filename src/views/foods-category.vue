8dca8e1fb99f1b859a86945f88b55f25d09c5792
konglingwen94 <794430774@qq.com>
2020-11-08 11:56:09 +0800

    优化

--- src/views/foods-category.vue
+++ src/views/foods-category.vue
@@ -156,7 +156,7 @@ export default {
     },
     async remove(id) {
       try {
-        await this.$confirm("此操作将永久删除该文件, 是否继续?", { type: "warning" });
+        await this.$confirm("此操作将永久删除该数据, 是否继续?", { type: "warning" });
       } catch (error) {
         return;
       }
