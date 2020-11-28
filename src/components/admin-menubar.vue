498518ccc351899a4ecdea2daf758661c20bad46
konglingwen94 <794430774@qq.com>
2020-11-28 12:29:01 +0800

    添加管理员设置页面

--- src/components/admin-menubar.vue
+++ src/components/admin-menubar.vue
@@ -11,7 +11,7 @@
           <i class="el-icon-platform-eleme"></i>
           <span slot="title">店铺管理</span>
         </template>
-        <el-menu-item index="/seller/setting"> <i class="el-icon-s-operation"></i>店铺设置 </el-menu-item>
+        <el-menu-item index="/seller/setting"> <i class="el-icon-s-operation"></i>店铺配置 </el-menu-item>
         <el-menu-item index="/seller/dashboard"> <i class="el-icon-s-data"></i>店铺统计 </el-menu-item>
       </el-submenu>
 
@@ -38,7 +38,7 @@
         <span slot="title">评价管理</span>
       </el-menu-item>
 
-      <el-menu-item index="/4">
+      <el-menu-item index="/setting">
         <i class="el-icon-setting"></i>
         <span slot="title">设置</span>
       </el-menu-item>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -15,43 +14,49 @@ const routes = [
     path: "/foods-catefory",
     name: "foods-catefory",
 
-    component: () => import(/* webpackChunkName: "foods-category" */ "../views/foods-category.vue"),
+    component: () => import(/* webpackChunkName: "foods-category" */ "@/views/foods-category.vue"),
   },
   {
     path: "/foods-list",
     name: "foods-list",
 
-    component: () => import(/* webpackChunkName: "foods-list" */ "../views/foods-list.vue"),
+    component: () => import(/* webpackChunkName: "foods-list" */ "@/views/foods-list.vue"),
   },
   {
     path: "/foods/:id/edit",
     name: "foods-editor",
 
-    component: () => import(/* webpackChunkName: "foods-editor" */ "../views/foods-editor.vue"),
+    component: () => import(/* webpackChunkName: "foods-editor" */ "@/views/foods-editor.vue"),
   },
   {
     path: "/foods/add",
-    name: "foods-editor",
+    name: "foods-add",
 
-    component: () => import(/* webpackChunkName: "foods-editor" */ "../views/foods-editor.vue"),
+    component: () => import(/* webpackChunkName: "foods-editor" */ "@/views/foods-editor.vue"),
   },
   {
     path: "/ratings",
     name: "ratings",
 
-    component: () => import(/* webpackChunkName: "ratings" */ "../views/rating.vue"),
+    component: () => import(/* webpackChunkName: "ratings" */ "@/views/rating.vue"),
   },
   {
     path: "/seller/dashboard",
     name: "seller-dashboard",
 
-    component: () => import(/* webpackChunkName: "seller-dashboard" */ "../views/seller-dashboard.vue"),
+    component: () => import(/* webpackChunkName: "seller-dashboard" */ "@/views/seller-dashboard.vue"),
   },
   {
-    path: "/seller/setting",
-    name: "seller-setting",
+    path: "/seller/configuration",
+    name: "seller-configuration",
 
-    component: () => import(/* webpackChunkName: "seller-setting" */ "../views/seller-setting.vue"),
+    component: () => import(/* webpackChunkName: "seller-configuration" */ "@/views/seller-configuration.vue"),
+  },
+  {
+    path: "/setting",
+    name: "setting",
+
+    component: () => import(/* webpackChunkName: "setting" */ "@/views/setting.vue"),
   },
   {
     path:'*',
