ff878a997d5ce8017f87c9dcf5e1b2a40c4dda65
konglingwen94 <794430774@qq.com>
2020-12-07 17:10:09 +0800

    添加路由根路径

--- src/router/index.js
+++ src/router/index.js
@@ -87,7 +87,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  // base: process.env.BASE_URL,
+  base: process.env.BASE_URL,
   routes: [
     {
       name: "authLayout",
